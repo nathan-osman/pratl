1faa80d87daeafcc7326a9e360b4c21f0d37d784
Nathan Osman <nathan@quickmediasolutions.com>
2016-02-05 21:42:18 -0800

    Added NewChannel function.

--- models/channel.go
+++ models/channel.go
@@ -16,3 +16,16 @@ type Channel struct {
 func init() {
 	orm.RegisterModel(new(Channel))
 }
+
+// NewChannel creates a new channel with the specified owner.
+func NewChannel(name, description string, owner *User) (c *Channel, err error) {
+	c = &Channel{
+		Name:        name,
+		Description: description,
+		Owners: []*User{
+			owner,
+		},
+	}
+	_, err = orm.NewOrm().Insert(c)
+	return
+}
