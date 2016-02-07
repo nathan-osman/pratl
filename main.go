644d556d720cb1b534e54be3fffdc2cee22c83e3
Nathan Osman <nathan@quickmediasolutions.com>
2016-02-07 00:57:30 -0800

    Added main.go.

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/lib/pq"
+
+	_ "github.com/nathan-osman/pratl/models"
+	_ "github.com/nathan-osman/pratl/routers"
+
+	"fmt"
+)
+
+func init() {
+	orm.RegisterDataBase(
+		"default",
+		"postgres",
+		fmt.Sprintf(
+			"postgres://%s:%s@%s:%s/%s",
+			beego.AppConfig.String("db_user"),
+			beego.AppConfig.String("db_pass"),
+			beego.AppConfig.DefaultString("db_host", "127.0.0.1"),
+			beego.AppConfig.DefaultString("db_port", "5432"),
+			beego.AppConfig.String("db_name"),
+		),
+	)
+}
+
+func main() {
+	beego.Run()
+}
