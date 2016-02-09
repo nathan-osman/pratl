eb2434862633584238390065513a2afe380b105f
Nathan Osman <nathan@quickmediasolutions.com>
2016-02-09 12:32:06 -0800

    Added filters for authentication.

--- routers/router.go
+++ routers/router.go
@@ -2,9 +2,28 @@ package routers
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
 
 	"github.com/nathan-osman/pratl/controllers"
+	"github.com/nathan-osman/pratl/models"
 )
+
+// FilterUser determines if the user is logged in.
+func FilterUser(ctx *context.Context) {
+	if id, ok := ctx.Input.Session("user_id").(int64); ok {
+		if u, err := models.GetUser(id); err == nil {
+			ctx.Input.SetData("user", u)
+		}
+	}
+}
+
+// FilterAuth prevents the user from accessing protected pages if they are not
+// logged in.
+func FilterAuth(ctx *context.Context) {
+	if ctx.Input.GetData("user") == nil {
+		ctx.Redirect(302, beego.URLFor("UserController.Login"))
+	}
+}
 
 func init() {
 	beego.Router("/", &controllers.IndexController{})
@@ -13,4 +32,7 @@ func init() {
 	beego.Router("/channels/:id:int/.*", &controllers.ChatController{}, "get:View")
 	beego.Router("/users/login", &controllers.UserController{}, "get,post:Login")
 	beego.Router("/users/register", &controllers.UserController{}, "get,post:Register")
+
+	beego.InsertFilter("/*", beego.BeforeRouter, FilterUser)
+	beego.InsertFilter("/channels/*", beego.BeforeRouter, FilterAuth)
 }
