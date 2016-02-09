89101570fadb3ca0d8dbef1e2b7c06e116362b8d
Nathan Osman <nathan@quickmediasolutions.com>
2016-02-09 11:17:12 -0800

    Added redirect for users already logged in.

--- controllers/index.go
+++ controllers/index.go
@@ -9,8 +9,13 @@ type IndexController struct {
 	beego.Controller
 }
 
-// Get displays the home page, which provides a form for logging in.
+// Get displays the home page with links for logging in and registering. If the
+// user is already logged in, they are redirected to the channels page.
 func (c *IndexController) Get() {
-	c.TplName = "index.tpl"
-	c.Render()
+	if c.GetSession("user_id") == nil {
+		c.TplName = "index.tpl"
+		c.Render()
+	} else {
+		c.Redirect("/channels", 302)
+	}
 }
