d76fa57a5cb61635e2addc6a23a114ec68afb5a5
Nathan Osman <nathan@quickmediasolutions.com>
2021-10-13 16:04:57 -0700

    Add missing tags to Member struct.

--- db/member.go
+++ db/member.go
@@ -3,10 +3,10 @@ package db
 // Member represents a member of a specific room.
 type Member struct {
 	ID      int64 `json:"id"`
-	UserID  int64 `gorm:"not null"`
+	UserID  int64 `gorm:"not null" json:"-"`
 	User    *User `gorm:"constraint:OnDelete:CASCADE" json:"user"`
-	RoomID  int64 `gorm:"not null"`
+	RoomID  int64 `gorm:"not null" json:"-"`
 	Room    *Room `gorm:"constraint:OnDelete:CASCADE" json:"room"`
-	IsOwner bool  `gorm:"not null"`
-	IsAdmin bool  `gorm:"not null"`
+	IsOwner bool  `gorm:"not null" json:"is_owner"`
+	IsAdmin bool  `gorm:"not null" json:"is_admin"`
 }
