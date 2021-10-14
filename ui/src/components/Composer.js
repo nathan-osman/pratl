428065154abb45a9e4e572c74028b36ffbd865ba
Nathan Osman <nathan@quickmediasolutions.com>
2021-10-13 21:16:28 -0700

    Begin styling page components.

--- ui/src/components/Composer.js
+++ ui/src/components/Composer.js
@@ -1,7 +1,19 @@
 import './Composer.scss';
 
 const Composer = () => {
-  return <div id="composer">Hello, world!</div>;
+  return <div id="composer">
+    <div className="controls">
+      <input
+        type="text"
+        className="elem input"
+        autocomplete="off"
+      />
+      <button
+        type="button"
+        className="elem btn"
+      >Send</button>
+    </div>
+  </div>;
 };
 
 export default Composer;
--- ui/src/components/Rooms.js
+++ ui/src/components/Rooms.js
@@ -1,7 +1,18 @@
 import './Rooms.scss';
 
 const Rooms = () => {
-  return <div id="rooms"></div>;
+  return <div id="rooms">
+    <div className="room-list">
+      <div className="room">
+        <div className="name">The First Room</div>
+        <div className="time">5 minutes ago</div>
+      </div>
+      <div className="room active">
+        <div className="name">Room for Bob and Jessica</div>
+        <div className="time">1 day ago</div>
+      </div>
+    </div>
+  </div>;
 };
 
 export default Rooms;
